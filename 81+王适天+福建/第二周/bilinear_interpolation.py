fbda7054fce856240595e10c81de6daf91214f4d
WangST2 <WangST2@ATLBattery.com>
2021-07-03 13:36:42 +0800

    第二周作业修复只能转换3通道图片不能单通道图片转换的问题

--- 81+王适天+福建/第二周/bilinear_interpolation.py
+++ 81+王适天+福建/第二周/bilinear_interpolation.py
@@ -9,33 +9,35 @@ import time
 
 
 def bilinear_interpolation(src_img, target_shape):
-    h, w, channel = src_img.shape
+    shape = src_img.shape
+    h, w = shape[0], shape[1]
     dst_h, dst_w = target_shape[1], target_shape[0]
     if dst_h == h and dst_w == w:
         return src_img.copy()
-
-    empty_img = np.zeros((dst_h, dst_w, channel), src_img.dtype)
+    if len(shape) == 3:
+        empty_img = np.zeros((dst_h, dst_w, shape[2]), src_img.dtype)
+    else:
+        empty_img = np.zeros(target_shape, src_img.dtype)
     scale_x, scale_y = float(w) / dst_w, float(h) / dst_h
-    for c in range(channel):
-        for dst_y in range(dst_h):
-            src_y = (dst_y + 0.5) * scale_y - 0.5
-            for dst_x in range(dst_w):
-                src_x = (dst_x + 0.5) * scale_x - 0.5
+    for dst_y in range(dst_h):
+        src_y = (dst_y + 0.5) * scale_y - 0.5
+        for dst_x in range(dst_w):
+            src_x = (dst_x + 0.5) * scale_x - 0.5
 
-                src_x0 = int(np.floor(src_x))
-                src_y0 = int(np.floor(src_y))
+            src_x0 = int(np.floor(src_x))
+            src_y0 = int(np.floor(src_y))
 
-                src_x1 = min(src_x0 + 1, w - 1)
-                src_y1 = min(src_y0 + 1, h - 1)
-                r1 = (src_x1 - src_x) * img[src_y0, src_x0, c] + (src_x - src_x0) * img[src_y0, src_x1, c]
-                r2 = (src_x1 - src_x) * img[src_y1, src_x0, c] + (src_x - src_x0) * img[src_y1, src_x1, c]
-                empty_img[dst_y, dst_x, c] = ((src_y1 - src_y) * r1 + (src_y - src_y0) * r2).astype(int)
-
+            src_x1 = min(src_x0 + 1, w - 1)
+            src_y1 = min(src_y0 + 1, h - 1)
+            r1 = (src_x1 - src_x) * img[src_y0, src_x0] + (src_x - src_x0) * img[src_y0, src_x1]
+            r2 = (src_x1 - src_x) * img[src_y1, src_x0] + (src_x - src_x0) * img[src_y1, src_x1]
+            empty_img[dst_y, dst_x] = ((src_y1 - src_y) * r1 + (src_y - src_y0) * r2).astype(int)
     return empty_img
 
 
 def my_bilinear_interpolation(src_img, target_shape):
-    h, w, channel = src_img.shape
+    shape = src_img.shape
+    h, w = shape[0], shape[1]
     dst_h, dst_w = target_shape
     if dst_h == h and dst_w == w:
         return src_img.copy()
@@ -54,20 +56,22 @@ def my_bilinear_interpolation(src_img, target_shape):
 
     src_x1 = np.clip(src_x0 + 1, 0, w - 1)
     src_y1 = np.clip(src_y0 + 1, 0, h - 1)
-    r1 = (src_x1 - src_x)[..., np.newaxis] * src_img[src_y0, src_x0, :] + (src_x - src_x0)[..., np.newaxis] * src_img[
-                                                                                                              src_y0,
-                                                                                                              src_x1, :]
-    r2 = (src_x1 - src_x)[..., np.newaxis] * src_img[src_y1, src_x0, :] + (src_x - src_x0)[..., np.newaxis] * src_img[
-                                                                                                              src_y1,
-                                                                                                              src_x1, :]
-
-    return ((src_y1 - src_y)[..., np.newaxis] * r1 + (src_y - src_y0)[..., np.newaxis] * r2).astype(np.uint8)
+    if shape == 3:
+        r1 = (src_x1 - src_x)[..., np.newaxis] * src_img[src_y0, src_x0] + (src_x - src_x0)[..., np.newaxis] * src_img[
+            src_y0, src_x1]
+        r2 = (src_x1 - src_x)[..., np.newaxis] * src_img[src_y1, src_x0] + (src_x - src_x0)[..., np.newaxis] * src_img[
+            src_y1, src_x1]
+        return ((src_y1 - src_y)[..., np.newaxis] * r1 + (src_y - src_y0)[..., np.newaxis] * r2).astype(np.uint8)
+    else:
+        r1 = (src_x1 - src_x) * src_img[src_y0, src_x0] + (src_x - src_x0) * src_img[src_y0, src_x1]
+        r2 = (src_x1 - src_x) * src_img[src_y1, src_x0] + (src_x - src_x0) * src_img[src_y1, src_x1]
+        return ((src_y1 - src_y) * r1 + (src_y - src_y0) * r2).astype(np.uint8)
 
 
 if __name__ == '__main__':
-    img = cv2.imread("lenna.png")
+    img = cv2.imread("lenna.png", 0)
     start = time.time()
-    t1 = bilinear_interpolation(img, (800, 800))
+    # t1 = bilinear_interpolation(img, (800, 800))
     end1 = time.time()
     t2 = my_bilinear_interpolation(img, (800, 800))
     end2 = time.time()
--- 81+王适天+福建/第二周/nearest_interpolation.py
+++ 81+王适天+福建/第二周/nearest_interpolation.py
@@ -15,40 +15,42 @@ import numpy as np
 
 def nearest_interpolation(src_img, target_shape):
     th, tw = target_shape
-    h, w, channel = src_img.shape
+    shape = src_img.shape
+    h, w = shape[0], shape[1]
     if th == h and tw == w:
         return src_img.copy()
-    empty_img = np.zeros((th, tw, channel), src_img.dtype)
-    sh = th / h
-    sw = tw / w
-    scale_x, scale_y = float(w) / tw, float(h) / th
+    if len(shape) == 3:
+        empty_img = np.zeros((th, tw, shape[2]), src_img.dtype)
+    else:
+        empty_img = np.zeros(target_shape, src_img.dtype)
+    sh, sw = th / h, tw / w
     for i in range(th):
-        src_y = (i + 0.5) * scale_y - 0.5
+        y = int(i / sh)
         for j in range(tw):
-            src_x = (j + 0.5) * scale_x - 0.5
-            x = int(src_x / sh)
-            y = int(src_y / sw)
-            empty_img[i, j] = src_img[x, y]
+            x = int(j / sw)
+            empty_img[i, j] = src_img[y, x]
     return empty_img
 
 
 def my_nearest_interpolation(src_img, target_shape):
     th, tw = target_shape
-    h, w, channel = src_img.shape
+    h, w = src_img.shape
     if th == h and tw == w:
         return src_img.copy()
+
     x_index, y_index = np.meshgrid((np.arange(tw) * w / tw).astype(int), (np.arange(th) * h / th).astype(int))
     xy_index = np.concatenate((x_index[..., np.newaxis], y_index[..., np.newaxis]), axis=2)
-    return img[xy_index[:, :, 1], xy_index[:, :, 0], :]
+    return img[xy_index[..., 1], xy_index[..., 0]]
 
 
 if __name__ == '__main__':
-    img = cv2.imread("lenna.png")
+    img = cv2.imread("lenna.png", 0)
     start = time.time()
     resize_img = nearest_interpolation(img, (800, 800))
     end1 = time.time()
     interpolation = my_nearest_interpolation(img, (800, 800))
     end2 = time.time()
     print("循环耗时%.3f s,numpy向量加速耗时%.3f s" % (end1 - start, end2 - end1))
+    print("两种方法像素值总差异", np.sum(interpolation - resize_img))
     cv2.imshow("nearest", interpolation)
     cv2.waitKey(0)
