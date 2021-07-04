a4b8f36e56d638fbc7cc000faccea891874bacda
Genius_Tian <wst452871153@gmail.com>
2021-07-04 23:50:39 +0800

    第三周作业 修改直方均衡化bug

--- 81+王适天+福建/第三周/histogram_equalization.py
+++ 81+王适天+福建/第三周/histogram_equalization.py
@@ -5,29 +5,32 @@
 #    @Description 图像直方图均衡化
 import cv2
 import numpy as np
-from histogram import *
 
 
 def histogram_equalization(image):
     h, w = image.shape
     copy = image.copy()
-    hist = cv2.calcHist([image], [0], None, [256], [0, 256])
+    hist_input = cv2.calcHist([image], [0], None, [256], [0, 256])
     init = 0
+    hist_out = []
     for i in range(256):
-        init += (hist[i][0] / (h * w))
-        x_index, y_index = np.where(copy == i)
-        copy[x_index, y_index] = np.around(init * 256 - 1)
+        init += (hist_input[i][0] / (h * w))
+        print(i, init)
+        hist_out.append(np.around(max(0, init * 256 - 1)))
+    for y in range(h):
+        for x in range(w):
+            copy[y, x] = hist_out[copy[y, x]]
     return copy.astype(np.uint8)
 
 
 if __name__ == '__main__':
     image = cv2.imread(r"../resources/lenna.png", 0)
     equalization = histogram_equalization(image)
-    print(image)
+    # print(image)
     # print(equalization)
-    gray_histogram2(image)
-    # gray_histogram2(equalization)
+    hist = cv2.equalizeHist(image)
+    # print(hist - equalization)
+    cv2.imshow("opencv", hist)
     cv2.imshow("origin", image)
     cv2.imshow("e", equalization)
-
     cv2.waitKey(0)
