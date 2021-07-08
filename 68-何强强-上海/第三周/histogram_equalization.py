e16fcd03d8acb2afe82b5e1426c1b7c1982e5779
moheyibei <huanmengmie@163.com>
2021-07-08 22:44:11 +0800

    自实现的均衡化，问题待处理

--- 68-何强强-上海/第三周/histogram_equalization.py
+++ 68-何强强-上海/第三周/histogram_equalization.py
@@ -14,23 +14,42 @@ ranges:像素值的范围，一般为[0,255]表示0~255
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from collections import Counter
 
 
-def func():
-    pass
+def func(img):
+    h, w = img.shape
+    input_hist, _ = np.histogram(img, 256)
+    output_data = img.copy()
+    pis, q_list = [], []
+    for i in range(0, 256):
+        pi = input_hist[i] / (h * w)
+        pis.append(pi)
+        q = sum(pis) * 256 - 1
+        q = max(0, q)
+        q = min(255, q)
+        q_list.append(q)
+    # todo 怎样替换？
+    return output_data
+
 
 
 if __name__ == '__main__':
 
     src = cv2.imread("lenna.png")
     gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-    res = cv2.calcHist(images=[gray], channels=[0], mask=None, histSize=[256], ranges=[0, 256])
-    # 原始图像直方图
-    plt.bar(range(0, 256), res.ravel())
-    plt.title("origin hist")
-    plt.show()
+    # res = cv2.calcHist(images=[gray], channels=[0], mask=None, histSize=[256], ranges=[0, 256])
+    # # 原始图像直方图
+    # plt.bar(range(0, 256), res.ravel())
+    # plt.title("origin hist")
+    # plt.show()
     # cv2 均衡化
-    eq1 = cv2.equalizeHist(gray)
-    plt.hist(eq1.ravel(), 256)
+    # eq1 = cv2.equalizeHist(gray)
+    # plt.hist(eq1.ravel(), 256)
+    # plt.title("equalization by cv2")
+    # plt.show()
+
+    des = func(gray)
+    plt.hist(des.ravel(), 256)
     plt.title("equalization by cv2")
     plt.show()
