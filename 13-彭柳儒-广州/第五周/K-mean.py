da2e5aa37a8c26dd4a20c43cd1e452ef16b023d2
PLR <529817864@qq.com>
2021-07-31 18:07:37 +0800

    第五周作业

--- 13-彭柳儒-广州/第五周/K-mean.py
+++ 13-彭柳儒-广州/第五周/K-mean.py
@@ -21,6 +21,7 @@ label_pred = estimator.labels_  # 获取聚类标签
 x0 = X[label_pred == 0]
 x1 = X[label_pred == 1]
 x2 = X[label_pred == 2]
+
 plt.scatter(x0[:, 0], x0[:, 1], c="red", marker='o', label='label0')
 plt.scatter(x1[:, 0], x1[:, 1], c="green", marker='*', label='label1')
 plt.scatter(x2[:, 0], x2[:, 1], c="blue", marker='+', label='label2')
